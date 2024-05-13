0e01393b1fabb2327ffb2a54793c029bd9e190ca
Steve <steve@tucbox.com>
2024-05-13 18:50:46 +0100

    some additional function exercises

---
+++ countChars.js
@@ -0,0 +1,12 @@
+function countChars(str) {
+    let count = 0;
+    let Bcount = 0;
+    while (count < str.length) {
+        if (str[count] == "B") {
+            Bcount++;
+        }
+        count++;
+    }
+    return Bcount
+}
+console.log(countChars("Billy Bob Thornton likes to play Ball."));
